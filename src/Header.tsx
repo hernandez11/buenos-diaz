80abfbd2ff2a51da0e89d9506cd9bd0ec744a4f8
Angel Hernandez Diaz <ahernandez11diaz@gmail.com>
2025-11-22 10:02:48 -0800

    updated socialLink in header to navigate to external link with href

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,16 +1,17 @@
-import { useNavigate } from 'react-router-dom'
-
 export default function Header() {
-  const navigate = useNavigate()
-
   return (
     <>
       <p data-testid={'InfoNavLink'}>INFO.</p>
       <p data-testid={'MenuNavLink'}>MENU.</p>
       <p data-testid={'LogoNavLink'}>BUENOS DIAZ</p>
-      <p onClick={() => navigate('./')} data-testid={'SocialNavLink'}>
+      <a
+        href='https://instagram.com/_u/joshua._.hdz'
+        target='_blank'
+        rel='noopener noreferrer'
+        data-testid='SocialNavLink'
+      >
         INSTA
-      </p>
+      </a>
     </>
   )
 }
--- src/tests/Header.test.tsx
+++ src/tests/Header.test.tsx
@@ -1,8 +1,6 @@
 import { describe, it, expect, vi } from 'vitest'
 import Header from '@/Header'
 import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import { useNavigate } from 'react-router-dom'
 
 vi.mock('react-router-dom', async () => {
   const actual = await vi.importActual('react-router-dom')
@@ -19,16 +17,9 @@ describe('Header', () => {
     expect(screen.getByTestId('InfoNavLink')).toBeInTheDocument()
     expect(screen.getByTestId('MenuNavLink')).toBeInTheDocument()
     expect(screen.getByTestId('LogoNavLink')).toBeInTheDocument()
-    expect(screen.getByTestId('SocialNavLink')).toBeInTheDocument()
-  })
 
-  it('navigates on social icon click', async () => {
-    const mockNavigate = vi.fn()
-    vi.mocked(useNavigate).mockReturnValue(mockNavigate)
-
-    render(<Header />)
-
-    await userEvent.click(screen.getByTestId('SocialNavLink'))
-    expect(mockNavigate).toHaveBeenCalledExactlyOnceWith('./')
+    const socialLink = screen.getByTestId('SocialNavLink')
+    expect(socialLink).toHaveAttribute('href', 'https://instagram.com/_u/joshua._.hdz')
+    expect(socialLink).toHaveAttribute('target', '_blank')
   })
 })
