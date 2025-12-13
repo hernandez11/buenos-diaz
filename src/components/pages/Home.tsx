83b40bb7a2027507ab1bc0795b81c7e24028e1c6
Angel Hernandez Diaz <ahernandez11diaz@gmail.com>
2025-12-13 01:03:36 -0500

    updated description

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -9,44 +9,42 @@ const menuList: Menu[] = [
     title: 'CARAMELO LATTE',
     subtitle: 'Goat-milk caramel | Brown sugar | Vanilla',
     description: `
-    Slow-cooked and deeply comforting, this latte highlights 
-    the warmth of cajeta, a 
-
-    classic Mexican caramel made from goat’s milk. 
-    Smooth and nostalgic, it’s a 
-
-    festive sip that feels both familiar and special.`,
+    Slow-cooked and deeply comforting,
+    
+    this latte highlights the warmth of cajeta, 
+    
+    a classic Mexican caramel made from goat’s milk
+    
+    Smooth and nostalgic, it’s a festive sip that feels both familiar and special`,
   },
   {
     title: 'PONCHE NAVIDEÑO LATTE',
     subtitle: 'Guava | Orange | Sugarcane',
-    description: `A tribute to the Christmas season in 
-
-    Mexico, this latte draws from the flavors of traditional 
-    ponche enjoyed on cold nights. 
-    Aromatic, fruity, and warming, it’s 
+    description: `A tribute to the Christmas season in Mexico,
     
-    festive without being sweet-heavy, and deeply tied to holiday ritual.`,
+    this latte draws from the flavors of traditional ponche enjoyed on cold nights
+
+    Aromatic, fruity, and warming, it’s festive without being sweet-heavy, and deeply tied to holiday ritual`,
   },
   {
     title: 'ROMPOPE LATTE',
     subtitle: 'Homemade eggnog | Cinnamon | Brown sugar',
-    description: `Inspired by traditional rompope shared during the 
+    description: `Inspired by traditional rompope shared during the holidays, 
     
-    holidays, this latte is rich, smooth, 
+    this latte is rich, smooth, and deeply comforting
     
-    and deeply comforting. A festive drink rooted in warmth, 
-    tradition, and celebration.`,
+    A festive drink rooted in warmth, 
+    tradition, and celebration`,
   },
   {
     title: 'CAFÉ DE OLLA LATTE',
     subtitle: 'Piloncillo | Cinnamon | Orange',
-    description: `Steeped with aromatic spices, this
-  
-  latte captures the soul of traditional Mexican coffee.Inspired by
-  
-  the clay pot brews shared in rural kitchens, it’s warm, spiced, &
-  deeply rooted in heritage.`,
+    description: `Steeped with aromatic spices, 
+    
+    this latte captures the soul of traditional Mexican coffee
+    
+    Inspired by the clay pot brews shared in rural kitchens, it’s warm, spiced, &
+  deeply rooted in heritage`,
   },
 ]
 
@@ -62,7 +60,7 @@ export const Home = () => {
 
         My family has carried on this tradition for generations, cultivating and harvesting coffee with the same care and dedication. Growing up surrounded by these practices instilled in me a deep respect for the craft and a desire to share its warmth, richness, and cultural significance.
 
-        At Buenos Díaz, we specialize in specialty coffee and crafting experiences that go beyond the cup. From thoughtfully sourced beans to homemade syrups and flavors inspired by Mexican traditions, every drink tells a story—one of care, culture, and the joy of sharing coffee with others.`}</p>
+        At Buenos Díaz, we specialize in specialty coffee and crafting experiences that go beyond the cup. From thoughtfully sourced beans to homemade syrups and flavors grounded in Mexican traditions, every drink tells a story—one of care, culture, and the joy of sharing coffee with others.`}</p>
       </div>
       <div className='centerImageContainer'></div>
       <TooltipButton />
